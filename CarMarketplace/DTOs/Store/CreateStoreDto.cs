6731928959a2c6e65c3c25c7dec546637742a88a
DanNg05 <sydan05071997@gmail.com>
2024-10-19 20:46:27 +1030

    Store Dtos

---
+++ CarMarketplace/DTOs/Store/CreateStoreDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CarMarketplace.DTOs.Store
+{
+    public class CreateStoreDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
---
+++ CarMarketplace/DTOs/Store/UpdateStoreDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CarMarketplace.DTOs.Store
+{
+    public class UpdateStoreDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
